5c96be1e0274c58bae762c8f8ffc35a14459c254
MaxHolzmann <max@holzmann.io>
2023-05-24 14:00:44 -0500

    fixes extra small breakpoint

--- app/page.js
+++ app/page.js
@@ -43,20 +43,20 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className='flex items-center justify-center gap-2 p-12 sm:flex-col lg:flex-row'>
+        <div className='flex items-center justify-center gap-2 p-12 xsm: flex-col sm:flex-col lg:flex-row'>
           <img
             data-aos='fade-right'
             className='h-48 w-96 object-scale-down'
             src='/profile-pic.png'
           ></img>
 
-          <h1 data-aos='fade-left' className='text-8xl'>
+          <h1 data-aos='fade-left' className='text-8xl text-center'>
             Hi, Im Max.
           </h1>
         </div>
 
         <div className='flex flex-col items-center justify-center gap-2 p-4'>
-          <h2 data-aos='fade-up' className='p-5 text-4xl'>
+          <h2 data-aos='fade-up' className='p-5 text-4xl text-center'>
             I am a Full Stack Web Developer based out of Fond du Lac, Wisconsin.
           </h2>
         </div>
@@ -66,7 +66,7 @@ export default function Home() {
         <h1
           data-aos='fade-up'
           data-aos-anchor-placement='top-center'
-          className='text-8xl'
+          className='text-8xl text-center'
         >
           This is test text.
         </h1>
@@ -74,7 +74,7 @@ export default function Home() {
         <h1
           data-aos='fade-up'
           data-aos-anchor-placement='top-center'
-          className='text-8xl'
+          className='text-8xl text-center'
         >
           This is test text.
         </h1>
@@ -82,7 +82,7 @@ export default function Home() {
         <h1
           data-aos='fade-up'
           data-aos-anchor-placement='top-center'
-          className='text-8xl'
+          className='text-8xl text-center'
         >
           This is test text.
         </h1>
