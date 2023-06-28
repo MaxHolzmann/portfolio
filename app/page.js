e30c7d707421a4ca4dfab4fbf0ef0b5e0ac63122
MaxHolzmann <max@holzmann.io>
2023-06-28 14:26:38 -0500

    replaces filler texts and makes a text object instead of hard coding text in

--- app/page.js
+++ app/page.js
@@ -42,6 +42,17 @@ export default function Home() {
     document.body.style.backgroundColor = backgroundColor;
   }, [scrollPosition]);
 
+
+  const allText = {
+   "headerText": "Hi, I'm Max",
+   "headerDescription": "I am a Full Stack Web Developer based out of Fond du Lac, Wisconsin.",
+   "sectionOneHeader": "Passion",
+   "sectionOneText": "I pour my passion into every aspect of developing websites. From the initial concept to the final launch, I infuse my work with enthusiasm and dedication. With meticulous attention to detail, I craft visually captivating designs and seamless functionalities that engage and delight users.",
+   "sectionTwoHeader": "Purpose",
+   "sectionTwoText": "As a purpose-driven web developer, I am fueled by a deep commitment to building websites that serve a meaningful mission. Every line of code I write is guided by a clear purpose and a desire to make a positive impact.  I invest time in understanding the needs of my clients or users, ensuring that the websites I create are tailored to their goals and aspirations.",
+   "sectionThreeHeader": "Projects",
+   "sectionThreeText": "Showcasing projects",
+  }
 /* 
 TODO: 
 Add divs for each text block to handle padding / margin between each instead of applying margin to each text item
@@ -63,13 +74,13 @@ Potentially change opacity of scroll and change background
           ></img>
 
           <h1 data-aos='fade-left' className='text-8xl text-center'>
-            Hi, Im Max.
+           {allText.headerText}
           </h1>
         </div>
 
         <div className='flex flex-col items-center justify-center gap-2 p-4'>
           <h2 data-aos='fade-up' className='p-5 text-4xl text-center'>
-            I am a Full Stack Web Developer based out of Fond du Lac, Wisconsin.
+            {allText.headerDescription}
           </h2>
         </div>
       </main>
@@ -80,11 +91,11 @@ Potentially change opacity of scroll and change background
           data-aos-anchor-placement='top-center'
           className='text-8xl text-center'
         >
-          Passion
+          {allText.sectionOneHeader}
         </h1>
 
         <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='py-10 text-4xl text-center'>
-        Blah blah blah
+        {allText.sectionOneText}
           </h2>
 
         <h1
@@ -92,11 +103,11 @@ Potentially change opacity of scroll and change background
           data-aos-anchor-placement='top-center'
           className='text-8xl text-center py-20'
         >
-          Purpose
+          {allText.sectionTwoHeader}
         </h1>
 
         <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='text-4xl text-center'>
-        Blah blah blah
+       {allText.sectionTwoText}
           </h2>
 
 
@@ -106,11 +117,11 @@ Potentially change opacity of scroll and change background
           data-aos-anchor-placement='top-center'
           className='py-20 text-8xl text-center'
         >
-          Projects
+          {allText.sectionThreeHeader}
         </h1>
 
         <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='py-10 text-4xl text-center'>
-        Blah blah blah
+        {allText.sectionThreeText}
           </h2>
 
       </div>
