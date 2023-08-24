02e1d790f2a6aaf1252c95bffd8f8b54359ff79d
MaxHolzmann <max@holzmann.io>
2023-08-24 15:28:30 -0500

    adjusts colors on text and background of hero section, adds notes in comments

--- app/page.js
+++ app/page.js
@@ -25,45 +25,47 @@ export default function Home() {
     };
   }, []);
 
-
   const allText = {
-   "headerText": "Hi, I'm Maximilian",
-   "headerName": "Maximilian",
-   "headerDescription": "Full Stack Web Developer",
-   "sectionOneHeader": "Passion",
-   "sectionOneText": "I pour my passion into every aspect of developing websites. From the initial concept to the final launch, I infuse my work with enthusiasm and dedication. With meticulous attention to detail, I craft visually captivating designs and seamless functionalities that engage and delight users.",
-   "sectionTwoHeader": "Experience",
-   "sectionTwoText": "Write about my experience here",
-   "sectionThreeHeader": "Projects",
-   "sectionThreeText": "Showcasing projects",
+    "headerText": "Hi, I'm Maximilian",
+    "headerName": "Maximilian",
+    "headerDescription": "Full Stack Web Developer",
+    "sectionOneHeader": "Passion",
+    "sectionOneText": "I pour my passion into every aspect of developing websites. From the initial concept to the final launch, I infuse my work with enthusiasm and dedication. With meticulous attention to detail, I craft visually captivating designs and seamless functionalities that engage and delight users.",
+    "sectionTwoHeader": "Experience",
+    "sectionTwoText": "Write about my experience here",
+    "sectionThreeHeader": "Projects",
+    "sectionThreeText": "Showcasing projects",
   }
-/* 
-TODO: 
-Add divs for each text block to handle padding / margin between each instead of applying margin to each text item
-Fix border on profile image 
-Change profile image
-Change background
-Potentially change opacity of scroll and change background
-*/
+  /* 
+  TODO: 
+  Add divs for each text block to handle padding / margin between each instead of applying margin to each text item
+  Change profile image
+
+  Potential snap idea where each section takes up full view of screen, changing color backgrounds etc.
+  Incorporate more images versus the wall of text.
+  redo some of the text.
+
+  */
 
 
   return (
     <div>
       <main className="flex flex-col items-center">
-        <div data-aos='fade-down' className='w-3/4 flex items-center justify-center gap-2 p-16 my-5 rounded-xl shadow-xl xsm: bg-slate-300 flex-col sm:flex-col lg:flex-row'>
+        <div data-aos='fade-down' className='w-3/4 flex items-center justify-center gap-2 p-16 my-5 rounded-xl shadow-xl xsm: bg-gray-600 flex-col sm:flex-col lg:flex-row'>
           <img
             data-aos='fade-right'
             className='h-64'
             src='/profile-pic.png'
           ></img>
 
+
           <h1 data-aos='fade-left' className='lg:text-8xl text-center text-6xl sm:mt-10 lg:mt-0 text-white'>
-           {allText.headerText}
+            {allText.headerText}
           </h1>
         </div>
 
         <div className='flex flex-col items-center justify-center gap-2 p-10 mx-40  my-2'
-        data-aos='fade-up'>
+          data-aos='fade-up'>
           <h2 class="animate-text text-center bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-7xl font-semibold">{allText.headerDescription}</h2>
 
         </div>
@@ -78,9 +80,9 @@ Potentially change opacity of scroll and change background
           {allText.sectionOneHeader}
         </h1>
 
-        <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='py-10 text-4xl text-center'>
-        {allText.sectionOneText}
-          </h2>
+        <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='py-10 text-gray-600 text-4xl text-center'>
+          {allText.sectionOneText}
+        </h2>
 
         <h1
           data-aos='fade-up'
@@ -91,8 +93,8 @@ Potentially change opacity of scroll and change background
         </h1>
 
         <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='text-4xl text-center'>
-       {allText.sectionTwoText}
-          </h2>
+          {allText.sectionTwoText}
+        </h2>
 
 
 
@@ -105,8 +107,8 @@ Potentially change opacity of scroll and change background
         </h1>
 
         <h2 data-aos='fade-up' data-aos-anchor-placement='top-center' className='py-10 text-4xl text-center'>
-        {allText.sectionThreeText}
-          </h2>
+          {allText.sectionThreeText}
+        </h2>
 
       </div>
     </div>
