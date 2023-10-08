8b069f3f754d8cf1943d6b742865e979e0d09f4b
Maximilian Holzmann <max@holzmann.io>
2023-10-08 10:17:32 -0500

    begins portfolio redesign AGAIN, this time I have a good idea of what I want to do.

--- app/redesign/page.js
+++ app/redesign/page.js
@@ -6,16 +6,10 @@ import "aos/dist/aos.css";
 //add a dark mode toggle for fun! :D
 
 export default function Home() {
-  //Initiates AOS for Scrolling animations
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
-
   const allText = {
     headerText: "Hi, I'm Maximilian.",
     headerName: "Maximilian",
-    headerDescription: "Full Stack Web Developer",
+    headerDescription: "Full Stack Web Developer 💻",
     sectionOneHeader: "Experience",
     sectionOneText:
       "I am a graduate of University of Minnesota's Full Stack Web Development Bootcamp. I have also completed a Web Development internship with HomeTownTickets.com. I am proficient in JavaScript and have experience with React, TailwindCSS, Node, Express, MongoDB, MySQL, Next.js & more.",
@@ -26,54 +20,17 @@ export default function Home() {
 
   return (
     <>
-      <header>
-        <h1
-          data-aos='fade-down'
-          className='m-10 text-center text-7xl font-semibold'
-        >
+      <header className='flex h-screen flex-col place-items-center bg-gradient-to-b from-orange-400'>
+        <h1 className='mt-10 text-center text-7xl font-bold text-white'>
           {allText.headerText}
         </h1>
-        <h2
-          data-aos='fade-up'
-          class='animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-center text-6xl font-semibold text-transparent'
-        >
+        <h2 className='p-4 text-3xl font-semibold text-white'>
           {allText.headerDescription}
         </h2>
       </header>
 
-      <section className='flex flex-col items-center justify-center'>
-        <h1
-          data-aos='fade-right'
-          className='mt-14 text-5xl font-bold text-gray-700'
-        >
-          {allText.sectionOneHeader}
-        </h1>
-        <p
-          data-aos='fade-left'
-          className='mt-5 px-20 text-center text-2xl font-semibold'
-        >
-          {allText.sectionOneText}
-        </p>
-        <img
-          data-aos='fade-up'
-          className='mt-10 rounded-2xl shadow-lg '
-          src='/certificate.png'
-        ></img>
-      </section>
-
-      <section className='flex flex-col items-center justify-center'>
-        <h1
-          data-aos='fade-right'
-          className='mt-14 text-5xl font-bold text-gray-700'
-        >
-          {allText.sectionTwoHeader}
-        </h1>
-        <p
-          data-aos='fade-left'
-          className='mt-5 px-20 text-center text-2xl font-semibold'
-        >
-          {allText.sectionTwoText}
-        </p>
+      <section>
+        <h1>About Me</h1>
       </section>
     </>
   );
