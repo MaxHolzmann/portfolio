b832f7f95f60f251b023de786d123f5555b986a6
MaxHolzmann <max@holzmann.io>
2024-07-25 14:35:26 -0500

    adds blank target to project links

--- app/components/Project.js
+++ app/components/Project.js
@@ -5,31 +5,31 @@ export default function Project({
   deployment,
   github,
 }) {
-
   // can i make the title more intersting? or visually appealing?
   // make buttons more interesting?
 
   return (
-    <div className='m-3 w-3/4 rounded-lg bg-white p-3 shadow-md'>
-
-      <h1 className='pb-5 text-center text-slate-500 font-sans text-2xl md:text-3xl lg:text-4xl drop-shadow-md'>
+    <div className="m-3 w-3/4 rounded-lg bg-white p-3 shadow-md">
+      <h1 className="pb-5 text-center font-sans text-2xl text-slate-500 drop-shadow-md md:text-3xl lg:text-4xl">
         {title}
       </h1>
 
-      <div className='grid grid-cols-1 gap-10 md:grid-cols-2'>
-        <img className='rounded-xl shadow-lg' src={image} />
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <img className="rounded-xl shadow-lg" src={image} />
 
-        <div className='flex flex-col items-center justify-center'>
-          <p className='lg:text-lg'>
-            {description}
-          </p>
+        <div className="flex flex-col items-center justify-center">
+          <p className="lg:text-lg">{description}</p>
 
-          <div className='m-3 flex gap-4'>
-            <button className='rounded bg-slate-500 p-3 font-semibold text-white hover:scale-105 drop-shadow-sm'>
-              <a href={deployment}>Deployment</a>
+          <div className="m-3 flex gap-4">
+            <button className="rounded bg-slate-500 p-3 font-semibold text-white drop-shadow-sm hover:scale-105">
+              <a target="_blank" href={deployment}>
+                Deployment
+              </a>
             </button>
-            <button className='rounded bg-slate-500 p-3 font-semibold text-white hover:scale-105 drop-shadow-sm'>
-              <a href={github}>Github</a>
+            <button className="rounded bg-slate-500 p-3 font-semibold text-white drop-shadow-sm hover:scale-105">
+              <a target="_blank" href={github}>
+                Github
+              </a>
             </button>
           </div>
         </div>
