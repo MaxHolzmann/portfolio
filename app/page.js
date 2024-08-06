9722cf85ce0bb20477136db3ca7f0f03084d0738
MaxHolzmann <max@holzmann.io>
2024-08-06 13:54:58 -0500

    slightly changes the gradient

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
 
   return (
     <>
-      <header className="sm: flex h-screen flex-col place-items-center justify-center bg-gradient-radial from-orange-500 via-orange-500 to-white to-70% pb-96 pt-52 text-center sm:p-10 sm:pt-0">
+      <header className="sm: flex h-screen flex-col place-items-center justify-center bg-gradient-to-b from-orange-500 via-orange-400 to-white to-95% pb-96 pt-52 text-center sm:p-10 sm:pt-0">
         <h1 className="text-wrap mt-8 text-center font-bebas text-8xl font-bold tracking-wide text-white drop-shadow-lg md:text-9xl">
           {allText.headerText}
         </h1>
