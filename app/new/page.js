2715ae74d077179da1387fe222d348cc7e329ed5
MaxHolzmann <max@holzmann.io>
2024-11-11 15:39:09 -0600

    adds old project cards to new design, intend to replace with a project carasol in the future

--- app/new/page.js
+++ app/new/page.js
@@ -1,10 +1,12 @@
 "use client";
 import { useRef, useEffect, useState } from "react";
+import Project from "../components/Project";
 import "aos/dist/aos.css";
 
 export default function Redesign() {
   const aboutMeRef = useRef();
   const [isInView, setIsInView] = useState(false);
+  //scale this with a function????
 
   const allText = {
     headerText: "Hi, I'm Maximilian.",
@@ -17,6 +19,47 @@ export default function Redesign() {
       "I am an active member in my community as well as an experienced business professional. I am a licensed real estate agent, real estate assistant & real estate transaction coordinator. I also serve on the Board of Directors for South Shore Chorale, and previously served on the Board of Directors for United Way Fond du Lac.",
     sectionOneThirdText:
       "I am seeking a full-time position as a Front End or Full Stack Web Developer. I believe my strong communication skills and professional experience would make a valuable asset to any team.",
+  };
+
+  const projectGameLog = {
+    title: "Game-Log",
+    description:
+      'Game-Log is a web application for keeping track of your gaming backlog. Utilizing a kanban board style list, users can add games, remove games and categorize games between three lists, "Back Log", "Currently Playing" and "Completed". This project was built with ReactJS & TailwindCSS, utilizing MongoDB as a database solution and NextAuth as an authentication solution.',
+    image: "/gamelog.png",
+    deployment: "https://game-log.vercel.app/",
+    github: "https://github.com/MaxHolzmann/game-log",
+  };
+  const projectMaxBlog = {
+    title: "Max Blog",
+    description:
+      "A basic blog application built with Handlebars, BootstrapCSS and MySQL / Sequelize.",
+    image: "/maxblog.png",
+    deployment: "https://max-blog.herokuapp.com/",
+    github: "https://github.com/MaxHolzmann/maxs-blog",
+  };
+  const projectBlueberries = {
+    title: "Blueberries Music",
+    description:
+      "A landing page for a local barbershop quartet that I am apart of. This website utilizies primarily React and TailwindCSS, and is deployed via Vercel.",
+    image: "/blueberries.jpg",
+    deployment: "https://www.blueberriesmusic.com/",
+    github: "https://github.com/MaxHolzmann/blueberriesmusic",
+  };
+  const projectHourLog = {
+    title: "Hour Log",
+    description:
+      'A full stack web application created using React, TailwindCSS, Next.JS, MongoDB & NextAuth. This web application is a simple solution for tracking work hours for companies who have employees track their own hours. Utilizing the "react-to-print" library, users can select a range of dates & export their hours into a convienant file PDF for their employer.',
+    image: "/hourlog.png",
+    deployment: "https://hour-log-gamma.vercel.app/",
+    github: "https://github.com/MaxHolzmann/hour-log",
+  };
+  const projectTimeline = {
+    title: "Real Estate Timeline Generator",
+    description:
+      "A one page web app utiilizing React Form Hook, in coalition with React, TailwindCSS and NextJS. This web application asks the user to input information regarding the timelines in a Real Estate transaction and creates a branded PDF for the user to send to their client. I created this project for my own Real Estate practice & my Real Estate team, The Schiek Team.",
+    image: "/timeline.png",
+    deployment: "https://timeline-gen.vercel.app/",
+    github: "https://github.com/MaxHolzmann/real-estate-timeline-gen",
   };
 
   useEffect(() => {
@@ -81,16 +124,45 @@ export default function Redesign() {
             isInView ? "animate-fade-right" : ""
           }`}
         >
-          Experience
+          Projects
         </h1>
-        <div className="flex">
-          <div className="flex max-w-4xl flex-col justify-center">
-            <p className="pb-4 text-xl font-normal">{allText.sectionOneText}</p>
-            <p className="pb-4 text-xl font-normal">
-              {allText.sectionOneSecondText}
-            </p>
-            <p className="text-xl font-normal">{allText.sectionOneThirdText}</p>
-          </div>
+        {/* replace with project carasol? ;o */}
+        <div className="flex flex-col items-center justify-center">
+          <Project
+            title={projectGameLog.title}
+            description={projectGameLog.description}
+            image={projectGameLog.image}
+            deployment={projectGameLog.deployment}
+            github={projectGameLog.github}
+          ></Project>
+          <Project
+            title={projectHourLog.title}
+            description={projectHourLog.description}
+            image={projectHourLog.image}
+            deployment={projectHourLog.deployment}
+            github={projectBlueberries.github}
+          ></Project>
+          <Project
+            title={projectTimeline.title}
+            description={projectTimeline.description}
+            image={projectTimeline.image}
+            deployment={projectTimeline.deployment}
+            github={projectTimeline.github}
+          ></Project>
+          <Project
+            title={projectBlueberries.title}
+            description={projectBlueberries.description}
+            image={projectBlueberries.image}
+            deployment={projectBlueberries.deployment}
+            github={projectBlueberries.github}
+          ></Project>
+          <Project
+            title={projectMaxBlog.title}
+            description={projectMaxBlog.description}
+            image={projectMaxBlog.image}
+            deployment={projectMaxBlog.deployment}
+            github={projectMaxBlog.github}
+          ></Project>
         </div>
       </section>
     </>
